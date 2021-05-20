20a67b5d29e44d399688e111a10549d0bcb4069c
Camilo Poma <cpomaz@uni.pe>
2021-05-19 23:25:57 -0500

    formatting code

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                title="Dash Me",
-                external_stylesheets=[dbc.themes.DARKLY],
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.7'}]
-                )
+app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    title="Dash Me",
+    external_stylesheets=[dbc.themes.DARKLY],
+    meta_tags=[
+        {
+            "name": "viewport",
+            "content": "width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.7",
+        }
+    ],
+)
 server = app.server
--- index.py
+++ index.py
@@ -7,118 +7,163 @@ from app import server
 import plotly.graph_objects as go
 
 
-from apps import test,me,publications,blog
+from apps import test, me, publications, blog
 
-app.layout = html.Div([
-
-
-    html.Div([
-        dbc.NavbarSimple(
-            children=[
-                # collapse,
-                #toast,
-                dbc.NavLink("Social Media", href="/apps/me", id="page-1-link",),
-                dbc.NavLink("Publications", href="/apps/publications", id="page-2-link",),
-                dbc.NavLink("Some tests", href="/apps/test", id="page-3-link",),
-                dbc.NavLink("Blog", href="/apps/blog", id="page-4-link",),
-            ],
-            # fill=True,
-            brand="Dash Me - Camilo Poma",
-            brand_style ={"font-size": "1.55em",'font-family':'Comic Sans MS'},
-            color="primary",
-            dark=True,
-            style={"font-size": "1.55em",'font-family':'Comic Sans MS'}
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                dbc.NavbarSimple(
+                    children=[
+                        # collapse,
+                        # toast,
+                        dbc.NavLink(
+                            "Social Media",
+                            href="/apps/me",
+                            id="page-1-link",
+                        ),
+                        dbc.NavLink(
+                            "Publications",
+                            href="/apps/publications",
+                            id="page-2-link",
+                        ),
+                        dbc.NavLink(
+                            "Some tests",
+                            href="/apps/test",
+                            id="page-3-link",
+                        ),
+                        dbc.NavLink(
+                            "Blog",
+                            href="/apps/blog",
+                            id="page-4-link",
+                        ),
+                    ],
+                    # fill=True,
+                    brand="Dash Me - Camilo Poma",
+                    brand_style={"font-size": "1.55em", "font-family": "Comic Sans MS"},
+                    color="primary",
+                    dark=True,
+                    style={"font-size": "1.55em", "font-family": "Comic Sans MS"},
+                ),
+            ],  # className="row"
         ),
-    ], #className="row"
-    ),
-    dcc.Location(id='url', refresh=True),
-    html.Div(id='page-content'
-    , children=[
-
-        #html.Div(
-         #   html.Img(src=app.get_asset_url('itdc.jpg')))
-
-        ],
-    # style={"background":"#F5F5F5","padding":"0.7%"}
-    ),
-
-
-])
+        dcc.Location(id="url", refresh=True),
+        html.Div(
+            id="page-content",
+            children=[
+                # html.Div(
+                #   html.Img(src=app.get_asset_url('itdc.jpg')))
+            ],
+            # style={"background":"#F5F5F5","padding":"0.7%"}
+        ),
+    ]
+)
 
 
 portada = html.Div(
-    [   html.Br(),
-        html.H2 ('Welcome to Camilo Dash App',style={"textAlign": "center","font-family":"Comic Sans MS"}),
+    [
         html.Br(),
-        dbc.Row([
-          dbc.Col(html.Img(src=app.get_asset_url('itdc.jpg'),style={'height':'60%', 'width':'58%'}),xs=12, sm=12, md=11, lg=6, xl=6),
-          dbc.Col([html.H3('Contents',style={"textAlign": "center","font-family":"Comic Sans MS"}),
+        html.H2(
+            "Welcome to Camilo Dash App",
+            style={"textAlign": "center", "font-family": "Comic Sans MS"},
+        ),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Img(
+                        src=app.get_asset_url("itdc.jpg"),
+                        style={"height": "60%", "width": "58%"},
+                    ),
+                    xs=12,
+                    sm=12,
+                    md=11,
+                    lg=6,
+                    xl=6,
+                ),
+                dbc.Col(
+                    [
+                        html.H3(
+                            "Contents",
+                            style={
+                                "textAlign": "center",
+                                "font-family": "Comic Sans MS",
+                            },
+                        ),
                         dcc.Markdown(
-                                '''
+                            """
                                 
                                 * ***Social Media:*** All my contact information
                                 * ***publications:*** Se muestran filtros adicionales
                                 * ***Blog:*** Evolución de la ventas mensuales
                                 * ***Some plotly graphs:*** medición de adquisición y retención
-                                '''
-                            ),
-                   html.H3('Notions', style={"textAlign": "center","font-family":"Comic Sans MS"}),
-                   dcc.Markdown(
-                       '''
+                                """
+                        ),
+                        html.H3(
+                            "Notions",
+                            style={
+                                "textAlign": "center",
+                                "font-family": "Comic Sans MS",
+                            },
+                        ),
+                        dcc.Markdown(
+                            """
 
                        * ***CcC:*** Clientes con Comprea
                        * ***Mix de Categoría:*** Número promedio de categorías que compran los clientes
                        * ***Mix de Familia:*** Número promedio de familia que compran los clientes
                        * ***Profundidad:*** Número promedio de sku's que compran los clientes
-                       '''
-                   )
-
-
-                   ],xs=12, sm=12, md=11, lg=6, xl=6),
-        ],
-        # justify="center",no_gutters=True,
-
+                       """
+                        ),
+                    ],
+                    xs=12,
+                    sm=12,
+                    md=11,
+                    lg=6,
+                    xl=6,
+                ),
+            ],
+            # justify="center",no_gutters=True,
         ),
-
         # html.Br(),
         # html.Br(),
-        
-        html.H5('cpomaz@uni.pe',style={"textAlign": "center","font-family":"Comic Sans MS"}),
+        html.H5(
+            "cpomaz@uni.pe",
+            style={"textAlign": "center", "font-family": "Comic Sans MS"},
+        ),
         html.Br(),
-        html.H5('https://github.com/CamiloPZ',style={"textAlign": "center","font-family":"Comic Sans MS"}),
+        html.H5(
+            "https://github.com/CamiloPZ",
+            style={"textAlign": "center", "font-family": "Comic Sans MS"},
+        ),
         html.Br(),
-        html.H5('https://www.instagram.com/camilo_poma/',style={"textAlign": "center","font-family":"Comic Sans MS"}),
+        html.H5(
+            "https://www.instagram.com/camilo_poma/",
+            style={"textAlign": "center", "font-family": "Comic Sans MS"},
+        ),
         html.Br(),
-
-
-
-
     ]
-
-
-
 )
 
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == '/':
-         return portada
-     #   return me.layout
-    if pathname == '/apps/me':
+    if pathname == "/":
+        return portada
+    #   return me.layout
+    if pathname == "/apps/me":
         return me.layout
-    if pathname == '/apps/publications':
+    if pathname == "/apps/publications":
         return publications.layout
-    if pathname == '/apps/test':
+    if pathname == "/apps/test":
         return test.layout
     # if pathname == '/apps/evolutivo':
     #     return evolutivo.layout
-    if pathname == '/apps/blog':
+    if pathname == "/apps/blog":
         return blog.layout
     else:
         return portada
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
